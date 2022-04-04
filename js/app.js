2a2075ff6180c664d01b567ca3ecca91053720da
Léo Bultel <leo.bultel@free.fr>
2022-04-04 19:45:00 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+
+//ANIM CLICK
+
+// window.addEventListener('click',(e) => {
+
+
+// 	const rond = document.createElement('div');
+// 	rond.className = 'clickAnim';
+// 	rond.style.left = `${e.pageX-50}px`;
+// 	rond.style.top = `${e.pageY-50}px`;
+// 	document.body.appendChild(rond);
+
+// 	setTimeout(()=>{
+// 		rond.remove();
+// 	},1500)
+// })	
+
